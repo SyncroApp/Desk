ab83213849146060f02ec63acc65db5a8e36c051
HeroBuxx CI <me@herobuxx.me>
2025-01-08 20:43:22 +0000

    deploy: ff3873dc302d0ba2766b3b41471db1add43ac602

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('push', (event) => {
+    console.log('Push event received:', event);
+    const data = event.data.json();
+  
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/Desk/logo192.png',
+    });
+  });  
